9543feefd06ead0b2c556aa9db7df5f2570b4949
annajantzen <aejantz2015@gmail.com>
2024-03-21 13:32:14 -0500

    updated models

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,13 +3,35 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
+  await Tag.findAll({
+    attributes: ['id', 'tag_name'],
+    include: [{
+      model: Product,
+      attributes: ['id', 'product_name', 'price', 'stock', 'catergory_id'],
+      through: 'ProductTag',
+    }]
+  })
+  .then((parsedTagData) => {
+    res.json(parsedTagDate)
+  })
+  .catch((err) => {
+    res.json(err)
+  })
   // be sure to include its associated Product data
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
+  Tag.findByPk(req.params.id, {
+    include: [{
+      model: Product,
+      attributes: ['id', 'product_name', 'price', 'stock', 'catergory_id'],
+      through: 'ProductTag',
+    }]
+  })
+  
   // be sure to include its associated Product data
 });
 
