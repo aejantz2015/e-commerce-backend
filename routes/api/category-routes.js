a8e24013d2d23409f54066672fa2e8778cd45f7a
annajantzen <aejantz2015@gmail.com>
2024-03-21 09:47:22 -0500

    updated index and started working on category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,9 +3,15 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
-  // be sure to include its associated Products
+  await Category.findAll({
+    attributes: ['id', 'category_name'],
+    include: [{
+      model: Product,
+      attributes: ['id', 'product_name', 'price', 'stock', 'catergory_id']
+    }]
+  })
 });
 
 router.get('/:id', (req, res) => {
