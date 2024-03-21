e6fd042e096044591a7b5d01e7116e903b937b93
annajantzen <aejantz2015@gmail.com>
2024-03-21 12:59:33 -0500

    updated products routes)

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,14 +4,53 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
+  await Product.findAll({
+    attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+    include: [
+      {
+        model: Tag,
+        attributes: ['id', 'tag_name'],
+        through: ProductTag
+      },
+      {
+        model: Category,
+        attributes: ['id', 'category_name']
+      }
+    ]
+  })
+  .then((productData) => {
+    res.json(productData)
+  })
+  .catch((err) => {
+    res.json(err)
+  })
   // be sure to include its associated Category and Tag data
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
+  await Product.findByPk(req.params.id, {
+    include: [
+      {
+        model: Tag,
+        attributes: ['id', 'tag_name'],
+        through: ProductTag
+      },
+      {
+        model: Category,
+        attributes: ['id', 'category_name']
+      }
+    ]
+  })
+  .then((oneProduct) => {
+    res.json(oneProduct)
+  })
+  .catch((err) =>{
+    res.json(err)
+  })
   // be sure to include its associated Category and Tag data
 });
 
@@ -94,6 +133,18 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  let deletedProduct = Product.findByPk(req.params.id)
+  Product.destroy({
+    where: {
+      id: req.params.id,
+    }
+  })
+  .then((product) => {
+    res.json(`${deletedProduct} has been deleted`)
+  })
+  .catch((err) => {
+    res.json(err)
+  })
 });
 
 module.exports = router;
